835da2a0bcc1860dd4389fc273b97f3eabc4d747
tim.tang <tang.jilong@139.com>
2014-04-27 14:53:26 +0800

    [arctic-wolf] - Add authentication provider.

---
+++ wolf-app/security/authentication/authentication-provider.js
@@ -0,0 +1,104 @@
+define(function(require, exports, module) {
+
+
+    var Backbone = require('backbone');
+    var $ = require('$');
+    var _ = require('underscore');
+
+    var authenticationProvider = Backbone.Model.extend({
+
+        url: APP.WS_HOST + '/security',
+
+        initialize: function() {
+            //check for sessionStorage support
+            if (Storage && sessionStorage) {
+                this.supportStorage = true;
+            }
+        },
+
+        //---------------- Methods For Local Storage ----------------//
+        get: function(key) {
+            if (!this.supportStorage) {
+                return Backbone.Model.prototype.get.call(this, key);
+            }
+            var data = sessionStorage.getItem(key);
+            if (data && data[0] !== '{') {
+                return data;
+            }
+            return JSON.parse(data);
+        },
+
+
+        put: function(key, value) {
+            if (this.supportStorage) {
+                sessionStorage.setItem(key, value);
+            } else {
+                Backbone.Model.prototype.set.call(this, key, value);
+            }
+            return this;
+        },
+
+        remove: function(key) {
+            if (this.supportStorage) {
+                sessionStorage.removeItem(key);
+            } else {
+                Backbone.Model.prototype.unset.call(this, key);
+            }
+            return this;
+        },
+
+        clear: function() {
+            if (!this.supportStorage) {
+                return Backbone.Model.prototype.clear(this);
+            }
+            sessionStorage.clear();
+        },
+
+
+        //-------------------- Security APIs ------------------------//
+        authenticate: function(credentials) {
+            var self = this;
+            var authenticate = $.ajax({
+                url: this.url + '/authenticate',
+                data: credentials,
+                type: 'POST',
+            });
+
+            authenticate.done(function(resp) {
+                self.put('authenticated', true);
+                self.put('security-user', JSON.stringify(response.user));
+                if (!self.get('redirect-url')) {
+                    Backbone.history.navigate('#dashboard/', {
+                        trigger: true
+                    });
+                }
+                var redirectUrl = self.get('redirect-url');
+                self.remove('redirect-url');
+                return Backbone.history.navigate(redirectUrl, {
+                    trigger: true
+                });
+            });
+
+            authenticate.fail(function(resp, status) {
+                Backbone.history.navigate('#security/login', {
+                    trigger: true
+                });
+            });
+        },
+
+        signout: function(callback) {
+            var self = this;
+            var signout = $.ajax({
+                url: this.url + '/signout',
+                type: 'DELETE'
+            });
+
+            signout.done(function(resp) {
+                self.clear();
+                callback();
+            });
+        }
+    });
+
+    module.exports = new authenticationProvider()
+});
