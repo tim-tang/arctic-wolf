4964f678069928c3a55c6ae0a57791cb10065669
tim.tang <tang.jilong@139.com>
2014-05-02 18:41:46 +0800

    [arctic-wolf] - Remove unused js.

--- wolf-app/layout/layout-app.js
+++ wolf-app/layout/layout-app.js
@@ -1,7 +1,6 @@
 define(function(require, exports, module) {
 
     //- Import dependency js
-    require('jcookie');
     require('nanoscroller');
     require('sparkline');
     require('jquery-ui');
