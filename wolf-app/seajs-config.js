99786b7e16374e1d4988ac8e80fc800f49a5710a
tim.tang <tang.jilong@139.com>
2014-05-06 12:56:36 +0800

    [arctic-wolf] - Extract seajs config from index.html

---
+++ wolf-app/seajs-config.js
@@ -0,0 +1,34 @@
+seajs.config({
+    //paths: {
+    //    "wolf-app": "http://localhost:3000/wolf-app"
+    //},
+    alias: {
+        "$": "jquery/jquery/1.10.1/jquery.js",
+        "underscore": "underscore/1.4.4/underscore-debug.js",
+        "backbone": "backbone/1.0.0/backbone-debug.js",
+        "layoutmanager": "backbone.layoutmanager/0.9.5/backbone.layoutmanager.js",
+        "subroute": "backbone.subroute/0.4.2/backbone.subroute.js",
+        "nanoscroller": "jquery.nanoscroller/jquery.nanoscroller.js",
+        "sparkline": "jquery.sparkline/jquery.sparkline.min.js",
+        "gritter": "jquery.gritter/js/jquery.gritter.js",
+        "quicksearch":"jquery.quicksearch/jquery.quicksearch.js",
+        "moment": "bootstrap.daterangepicker/moment.min.js",
+        "daterangepicker": "bootstrap.daterangepicker/daterangepicker.js",
+        "datetimepicker": "bootstrap.datetimepicker/js/bootstrap-datetimepicker.min.js",
+        "select2": "jquery.select2/select2.min.js",
+        "bootstrap-slider": "bootstrap.slider/js/bootstrap-slider.js",
+        "modalEffects": "jquery.niftymodals/js/jquery.modalEffects.js",
+        "jquery-datatables": "jquery.datatables/jquery.datatables.min.js",
+        "datatables": "jquery.datatables/bootstrap-adapter/js/datatables.js",
+        "jasny": "jasny.bootstrap/extend/js/jasny-bootstrap.min.js",
+        "parsley": "jquery.parsley/2.0.0/parsley.js",
+        "switch": "bootstrap.switch/bootstrap-switch.min.js",
+        "multi-select": "jquery.multiselect/js/jquery.multi-select.js",
+        "bootstrap": "bootstrap/dist/js/bootstrap.min.js",
+        "bt-touchspin": "bootstrap.touchspin/bootstrap-touchspin/bootstrap.touchspin.js",
+        "app-main": "./app-main/app-main.js"
+    },
+
+    debug: true,
+    charset: 'utf-8'
+});
