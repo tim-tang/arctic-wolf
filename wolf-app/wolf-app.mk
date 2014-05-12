3f5f448a9b2a2ff9ccc9637149384d889d51dbb3
tim.tang <tang.jilong@139.com>
2014-05-12 12:33:51 +0800

    [arctic-wolf] - Add makefile to building module.

---
+++ wolf-app/wolf-app.mk
@@ -0,0 +1,15 @@
+mod-core=app-core 
+mod-common=app-common
+mod-security=security
+mod-layout=layout
+mod-dashboard=dashboard
+mod-user-mgmt=user-mgmt
+mod-user-group-mgmt=user-group-mgmt
+mod-criteria-mgmt=criteria-mgmt
+mod-privilege-mgmt=privilege-mgmt
+mod-role-mgmt=role-mgmt
+mod-vehicle-mgmt=vehicle-mgmt
+mod-generic-filter=generic-filter
+mod-main=app-main
+
+wolf-app=app-core app-common security layout dashboard user-mgmt user-group-mgmt criteria-mgmt privilege-mgmt role-mgmt vehicle-mgmt generic-filter app-main
